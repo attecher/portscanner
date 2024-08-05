3145da65b0eec87885f11cd98fc227a55a29b9d3
attecher <137871173+attecher@users.noreply.github.com>
2024-08-05 22:39:27 +0530

    Add files via upload

---
+++ portscanner.py
@@ -0,0 +1,27 @@
+
+
+import socket
+import termcolor
+
+
+def scan(target,ports):
+	print("\n" + "Scanning Start For " + str(target))
+	for port in range(1,ports):
+		scan_port(target,port)
+def scan_port(ip, port):
+	try:
+		sock = socket.socket()
+		sock.connect((ip,port))
+		print(termcolor.colored("[+] Port opened " +str(port), "green" ))
+		sock.close()
+	except:
+		pass
+
+targets = input("[*] Enter the ipaddress( put , in between for multiple ip's): ")
+ports = int(input("[*] Enter the number of ports : "))
+if ',' in targets:
+	print("[*] Scanning Multiple Targets")
+	for ip_add in targets.split(','):
+		scan(ip_add.strip(' '),ports)
+else:
+	scan(targets,ports)
